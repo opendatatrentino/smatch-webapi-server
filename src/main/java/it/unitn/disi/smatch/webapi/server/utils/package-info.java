2622d5fa26570cb7f1065c8c2e25b857df8b4189
Moaz Reyad <moazreyad@gmail.com>
2013-07-17 14:49:06 +0200

    Add package documentation.

---
+++ src/main/java/it/unitn/disi/smatch/webapi/server/utils/package-info.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright 2012-2013 University of Trento - Department of Information
+ * Engineering and Computer Science (DISI)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ *
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ ******************************************************************************/ 
+
+/**
+ * Some utility classes that help the server.
+ */
+package it.unitn.disi.smatch.webapi.server.utils;
